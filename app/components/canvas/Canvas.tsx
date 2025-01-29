2bb83980fd5560835ba7267a5522c9b9dd252e3c
meiy112 <maggie.weng112@gmail.com>
2025-01-28 23:47:35 -0800

    Updated canvas colour

--- app/components/canvas/Canvas.tsx
+++ app/components/canvas/Canvas.tsx
@@ -1,9 +1,9 @@
 const Canvas = () => {
   return (
     <div className="flex-1 h-[100%] flex items-center justify-center">
-      <div className="w-[91%] h-[92%] rounded-[var(--border-radius-large)] bg-[#1E1D22] overflow-hidden">
+      <div className="w-[91%] h-[92%] rounded-[var(--border-radius-large)] bg-[#1E1D22] overflow-hidden flex flex-col gap-y-[0.5em]">
         <div className="bg-[var(--canvas-menu-bg)] h-[3em] w-[100%]"></div>
-        <div className="bg-[var(--canvas-bg)] flex-1"></div>
+        <div className="bg-[var(--canvas-bg)] h-[100%] w-[100%] rounded-[8px]"></div>
       </div>
     </div>
   );
